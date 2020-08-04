f9790456a1a2732c2be6f69362b89e1a2cb6b432
lints <lints1995@gmail.com>
2020-08-04 15:48:43 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # AWS-s3-upload
-upload file or image to AWS s3
+
+## 安装依赖
+```shell
+  yarn or npm install
+```
+
+## 上传准备
+
+需要上传图片放入到目录/images内
+
+## 上传运行
+
+```shell
+  node index.js
+```
---
+++ index.js
@@ -0,0 +1,75 @@
+let AWS = require("aws-sdk"),
+  fs = require("fs");
+const { DataSync } = require("aws-sdk");
+
+const uploadImagePath = "./images/";
+
+// AWS s3 上传配置
+const config = {
+  accessKeyId: "AKIAQKEGZ6OOUZBAZHMK",
+  secretAccessKey: "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",
+  region: "cn-northwest-1",
+  albumBucketName: "nx-prd-dpbp",
+  domain: "amazonaws.com.cn",
+};
+AWS.config.update({
+  accessKeyId: config.accessKeyId,
+  secretAccessKey: config.secretAccessKey,
+  region: config.region,
+});
+let images = []; //存放图片源文件路径下图片数组
+
+function readFileList(path, filesList) {
+  let files = fs.readdirSync(path);
+  files.forEach(function (item, index) {
+    let stat = fs.statSync(path + item);
+    if (stat.isDirectory()) {
+      //递归读取文件
+      readFileList(path + item + "/", filesList);
+    } else {
+      let obj = {}; //定义一个对象存放文件的路径和名字
+      obj.path = path; //路径
+      obj.filename = item; //名字
+      filesList.push(obj);
+    }
+  });
+}
+let getFiles = {
+  //获取文件夹下的所有文件
+  getFileList: function (path) {
+    let filesList = [];
+    readFileList(path, filesList);
+    return filesList;
+  },
+  //获取文件夹下的所有图片
+  getImageFiles: function (path) {
+    let imageList = [];
+    this.getFileList(path).forEach((item) => {
+      imageList.push({
+        buffer: fs.readFileSync(item.path + item.filename),
+        filename: item.filename,
+      });
+    });
+    return imageList;
+  },
+};
+
+images = getFiles.getImageFiles(uploadImagePath); // 获取文件夹下的所有图片
+for (const value of images) {
+  let s3bucket = new AWS.S3();
+  s3bucket.createBucket(function () {
+    let params = {
+      Bucket: config.albumBucketName,
+      Key: value.filename,
+      Body: value.buffer,
+      ACL: "public-read",
+    };
+    s3bucket.upload(params, function (err, data) {
+      if (err) {
+        console.log("ERROR MSG: ", err);
+      } else {
+        console.log(data);
+      }
+    });
+  });
+}
